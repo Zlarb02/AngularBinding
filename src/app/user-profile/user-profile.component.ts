bc1bce6012c6ec4ba2f311c923f0c7456d0d7e3c
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-05-21 18:47:25 +0200

    done

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  public isHidden: boolean;
+
+  constructor() {
+    this.isHidden = true;
+  }
+
+  user = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  public hide() {
+    this.isHidden = !this.isHidden;
+  };
+
+}
